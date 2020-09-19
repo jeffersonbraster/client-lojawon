a38806e51a51446428c8c0306f0db5463233a360
Jefferson <jeffersonbraster@gmail.com>
2020-09-19 16:03:29 -0300

    instalando e configurando o storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,13 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Aprendendo NEXT JS',
+  description = 'TypesScript, Reactjs e Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo src="/img/logo.svg" alt="Aprendendo NEXT JS" />
-    <S.Title>Aprendendo NEXT JS</S.Title>
-    <S.Description>TypesScript, Reactjs e Styled Components</S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="um desenvolvedor de frente ao pc"
