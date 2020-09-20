2bbfbedb940c307499f7a134a825f7c233add7c5
Jefferson <jeffersonbraster@gmail.com>
2020-09-19 21:31:05 -0300

    Usando storybook essentials e controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs/register'
-  ]
+  addons: ['@storybook/addon-essentials']
 }
